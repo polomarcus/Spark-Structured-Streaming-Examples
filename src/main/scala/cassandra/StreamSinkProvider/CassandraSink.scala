0cf63916c1f19944f1dfe621cba88f84c58da035
Paul Leclercq <paleclercq@gmail.com>
2017-11-21 23:05:09 -0500

    doc: update comments/better logs

--- src/main/scala/cassandra/StreamSinkProvider/CassandraSink.scala
+++ src/main/scala/cassandra/StreamSinkProvider/CassandraSink.scala
@@ -21,7 +21,6 @@ class CassandraSink() extends Sink {
   import org.apache.spark.sql.functions._
 
   private def saveToCassandra(df: DataFrame) = {
-    println("Saving this DS to Cassandra")
     val ds = CassandraDriver.getDatasetForCassandra(df)
     ds.show() //Debug only
 
@@ -38,7 +37,7 @@ class CassandraSink() extends Sink {
    * converting to an RDD allows us to do magic.
    */
   override def addBatch(batchId: Long, df: DataFrame) = {
-    println(s"saveToCassandra batchId : ${batchId}")
+    println(s"CassandraSink - Datastax's saveToCassandra method -  batchId : ${batchId}")
     saveToCassandra(df)
   }
 
@@ -48,9 +47,12 @@ class CassandraSink() extends Sink {
     * should be done in the same transaction as the data linked to the offsets
     */
   private def saveKafkaMetaData(df: DataFrame) = {
-    val kafkaMetadata = df.groupBy($"partition").agg(max($"offset").cast(LongType).as("offset")).as[KafkaMetadata]
+    val kafkaMetadata = df
+      .groupBy($"partition")
+      .agg(max($"offset").cast(LongType).as("offset"))
+      .as[KafkaMetadata]
 
-    println("saveKafkaMetaData")
+    println("Saving Kafka Metadata (partition and offset per topic (only one in our example)")
     kafkaMetadata.show()
 
     kafkaMetadata.rdd.saveToCassandra(CassandraDriver.namespace,
--- src/main/scala/kafka/KafkaSink.scala
+++ src/main/scala/kafka/KafkaSink.scala
@@ -26,6 +26,10 @@ object KafkaSink {
 
   /**
       Console sink from Kafka's stream
+      +----+--------------------+-----+---------+------+--------------------+-------------+--------------------+
+      | key|               value|topic|partition|offset|           timestamp|timestampType|          radioCount|
+      +----+--------------------+-----+---------+------+--------------------+-------------+--------------------+
+      |null|[7B 22 72 61 64 6...| test|        0|    60|2017-11-21 22:56:...|            0|[Feel No Ways,Dra...|
     *
     */
   def debugStream(staticKafkaInputDS: Dataset[SimpleSongAggregationKafka]) = {
